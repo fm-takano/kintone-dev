4e81acf76e926aba68897ddc544a4cbe8432c068
ftakano <fuminori.takano@keio.co.jp>
2017-08-22 02:37:21 +0000

    ファイルの集約化

--- js/control_display_19.js
+++ js/control_display_19.js
@@ -80,12 +80,57 @@
                 targetRow.value[radioVal+'_登録状況_氏名_配偶者'].value = '';
                 targetRow.value[radioVal+'_登録状況_氏名_扶養親族'].value = '';
             }
-
         }
+        // 社番変更時のみ新社番での検索も実施
+        if(radioVal === '社番変更'){
+            empCode = targetRow.value[radioVal+'_従業員番号_変更後'].value;
 
-
+            query = '';
+            query += '従業員番号="' + empCode + '"';
+            query = encodeURIComponent(query);
+            appUrl = kintone.api.url('/k/v1/records') + '?app=' + empListId + '&query=' + query;
+    
+            xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("GET", appUrl, false);
+            xmlHttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xmlHttp.send(null);
+    
+            //取得したレコードをArrayに格納
+            respData = JSON.parse(xmlHttp.responseText);
+    
+            // 結果件数が1以上（1以外はありえないけど。。）
+            if (respData.records.length) {
+                targetRow.value[radioVal+'_登録状況_従業員番号_新'].value = respData.records[0].従業員番号.value;
+                targetRow.value[radioVal+'_登録状況_氏名_本人_新'].value = respData.records[0].氏名.value;
+            }
+            else {
+                // 結果件数が0
+                // 個人事業主登録状況テーブルの検索
+                query = '';
+                query += '個人支払先番号="' + empCode + '"';
+                query = encodeURIComponent(query);
+                appUrl = kintone.api.url('/k/v1/records') + '?app=' + freelanceListId + '&query=' + query;
+    
+                xmlHttp = new XMLHttpRequest();
+                xmlHttp.open("GET", appUrl, false);
+                xmlHttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                xmlHttp.send(null);
+    
+                //取得したレコードをArrayに格納
+                respData = JSON.parse(xmlHttp.responseText);
+    
+                // 結果件数が1以上（1以外はありえないけど。。）
+                if (respData.records.length) {
+                    targetRow.value[radioVal+'_登録状況_従業員番号_新'].value = respData.records[0].個人支払先番号.value;
+                    targetRow.value[radioVal+'_登録状況_氏名_本人_新'].value = respData.records[0].氏名.value;
+                }
+                else {
+                    targetRow.value[radioVal+'_登録状況_従業員番号_新'].value = '';
+                    targetRow.value[radioVal+'_登録状況_氏名_本人_新'].value = '';
+                }
+            }
+        }
         return event;
-
     }
 
     // 新規作成、詳細表示、編集で表示する    
@@ -99,6 +144,8 @@
         'app.record.edit.change.氏名変更_従業員番号',
         'app.record.create.change.社番変更_従業員番号',
         'app.record.edit.change.社番変更_従業員番号',
+        'app.record.create.change.社番変更_従業員番号_変更後',
+        'app.record.edit.change.社番変更_従業員番号_変更後',
     ];
 
     kintone.events.on(eventList, function(event) {
@@ -156,6 +203,11 @@
             rowData[radioVal+'_登録状況_氏名_本人'].disabled = true;
             rowData[radioVal+'_登録状況_氏名_配偶者'].disabled = true;
             rowData[radioVal+'_登録状況_氏名_扶養親族'].disabled = true;
+            //従業員番号変更のみ
+            if(radioVal === '社番変更'){
+                rowData[radioVal+'_登録状況_従業員番号_新'].disabled = true;
+                rowData[radioVal+'_登録状況_氏名_本人_新'].disabled = true;
+            }
         }
         return event;
     }
